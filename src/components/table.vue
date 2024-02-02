7eeac87ace8af3ff4ee1c30090c4dd9f73d35488
Melker Öberg <memleoberg@gmail.com>
2024-02-02 05:14:15 +0100

    batman style table

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,8 +1,10 @@
 
 <!-- i want to populate the html table once the incomeAndMetrics is updated from the fetch component -->
+
+
 <template>
-  <div class="table-wrapper" v-if="incomeAndMetrics.length > 0">
-    <table>
+  <div class="table-container" @click="toggleTable">
+    <table class="table">
       <caption>Table</caption>
       <thead>
         <tr>
@@ -13,7 +15,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(value, key) in incomeAndMetrics[0]" :key="key">
+        <tr v-for="(value, key) in incomeAndMetrics[0]" :key="key" v-show="!isMinimized || index === 0">
           <td>{{ key.toUpperCase() }}</td>
           <td v-for="data in incomeAndMetrics" :key="data.date + key">
             {{ formatNumber(data[key]) }}
@@ -23,36 +25,35 @@
     </table>
     <!-- Buttons here -->
   </div>
+  <button @click="addToLocalStorage()">ADD TO LOCAL</button>
+  <button @click="removeFromLocalStorage()">REMOVE LOCAL</button>
 </template>
 
 
 <script>
 
-// incomeAndMetrics recives Data received from EventBus, the structure of this array is [ { symbol: 'AAPL', date: '2020-12-31', ... }, { symbol: 'AAPL', date: '2019-12-31', ... }, ... ]
-// i want the keys of the first object in the array to be in the let most column in the HTML,
-// and the column to the right should consist of the values of the first object in the array
-// the next column should consist of the values of the second object in the array
 import { EventBus } from '../event-bus.js';
 export default {
   name: 'table',
   data() {
     return {
+      isMinimized: true,
       incomeAndMetrics: [],
     };
   },
   methods: {
+    toggleTable() {
+      this.isMinimized = !this.isMinimized;
+    },
     addToLocalStorage(stockData) {
       let savedStocks = JSON.parse(localStorage.getItem('savedStocks')) || [];
-      savedStocks.push(stockData);
+      savedStocks.push(this.incomeAndMetrics);
       localStorage.setItem('savedStocks', JSON.stringify(savedStocks));
-
     },
     removeFromLocalStorage(index) {
-      this.combinedData.splice(index, 1);
-      localStorage.setItem(
-        "savedStocks",
-        JSON.stringify(this.combinedData)
-      );
+      let savedStocks = JSON.parse(localStorage.getItem('savedStocks')) || [];
+      savedStocks.splice(index, 1);
+      localStorage.setItem('savedStocks', JSON.stringify(savedStocks));
     },
     formatNumber(value, currency) {
       currency = this.incomeAndMetrics[0].reportedCurrency;
@@ -97,54 +98,80 @@ export default {
 </script>
 
 <style scoped>
-
-.table-wrapper {
-  border: 1px solid #00fe48;
-  display: flex;
-  width: fit-content;
-  max-width: 90vw;
-  padding: 10px;
-  font-size: smaller;
-  font-family: inter;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Arial', sans-serif;
+  background-color: #f5f5f5;
+  color: #333;
 }
 
-table {
-  box-sizing: border-box;
-  background-color: var(--primary-color);
-  color: var(--text-color);
-  height: fit-content;
+body {
+  background-color: #f5f5f5;
+  color: #f5f5f5;
+  font-size: 16px;
+  line-height: 1.6;
+  padding: 20px;
+}
+
+.table-container {
   width: 100%;
-  max-width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow-x: auto;
+  margin: 20px 0;
+}
+
+.table-container {
+  cursor: pointer;
+  /* Indicates the table is clickable */
+}
+
+.table {
   border-collapse: collapse;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-th {
-  padding: 5px;
-  height: fit-content;
-  color: rgb(255, 255, 255);
-  text-align: center;
-}
-
-td {
-  padding: 5px;
-  box-sizing: border-box;
-  height: fit-content;
+.table th,
+.table td {
   max-width: 200px;
-  text-align: center;
-}
-
-td:nth-child(1) {
-  width: fit-content;
+  padding: 12px 15px;
   text-align: left;
+  border-bottom: 1px solid #ddd;
 }
 
-td:nth-child(even) {
-  background: var(--primary-color);
-  color: var(--text-color);
+.table th {
+  background-color: #3a99ff;
+  color: #ffffff;
+  font-weight: bold;
 }
 
-td:nth-child(odd) {
-  background: var(--tertiary-color);
-  color: var(--text-color);
+.table tr:nth-child(even) {
+  background-color: #e5e5e5;
+}
+
+.table tr:hover {
+  background-color: #ddd;
+}
+
+.table td {
+  font-weight: normal;
+}
+
+/* Additional Styling */
+.table-header {
+  background-color: #007bff;
+  color: #ffffff;
+}
+
+.table-header th {
+  font-size: 18px;
+}
+
+.highlight {
+  color: #0056b3;
+  font-weight: bold;
 }
 </style>
