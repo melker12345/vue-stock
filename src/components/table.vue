3c104f9344990289a134a114fe03277733a09e33
Melker Öberg <memleoberg@gmail.com>
2024-02-20 16:22:55 +0100

    :)

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,134 +1,133 @@
 <template>
-<div>
-    <div class="stock-table-container" v-for="(stockList, index) in stocks" :key="index">
-      <div class="responsive-table">
-        <div class="btn-con">
-          <button @click="toggleTable(stockList[0].Symbol)">
-            {{ isMinimized[stockList[0].Symbol] ? 'Maximize' : 'Minimize' }}
-          </button>
-          <button @click="addToLocalStorage(stockList[0])">ADD TO LOCAL</button>
-          <button @click="removeFromLocalStorage(index)">REMOVE LOCAL</button>
+  <div>
+      <div class="stock-table-container" v-for="(stockList, index) in stocks" :key="index">
+        <div class="responsive-table">
+          <div class="btn-con">
+            <button @click="toggleTable(stockList[0].Symbol)">
+              {{ isMinimized[stockList[0].Symbol] ? 'Maximize' : 'Minimize' }}
+            </button>
+            <button @click="addToLocalStorage(stockList[0])">ADD TO LOCAL</button>
+            <button @click="removeFromLocalStorage(index)">REMOVE LOCAL</button>
+          </div>
+          
+          <!-- Minimized version of the table -->
+          <table v-if="isMinimized[stockList[0].Symbol]" class="stock-table-minimized">
+            <thead>
+              <tr>
+                <th>{{ stockList[0].Symbol }} </th>
+                <th>PE Ratio</th>
+                <th>Debt To Equity</th>
+                <th>Dividend Yield</th>
+                <th>Revenue</th>
+                <th>Payout Ratio</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{{ stockList[0].Date }}</td>
+                <td>{{ formatNumber(stockList[0].PeRatio) }}</td>
+                <td>{{ formatNumber(stockList[0].DebtToEquity) }}</td>
+                <td>{{ formatNumber(stockList[0].DividendYield) }}</td>
+                <td>{{ formatNumber(stockList[0].Revenue) }}</td>
+                <td>{{ formatNumber(stockList[0].PayoutRatio) }}</td>
+              </tr>
+            </tbody>
+          </table>
+  
+          <!-- Maximized version of the table -->
+          <table v-else>
+            <thead>
+              <tr>
+                <th>{{ stockList[0].Symbol }}</th>
+                <th v-for="report in stockList" :key="report.Date">
+                  {{ report.Date }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+  
+              </tr>
+              <tr v-for="(value, key) in stockList[0]" :key="key">
+                <td>{{ key }}</td>
+                <td v-for="(report, reportIndex) in stockList" :key="reportIndex">
+                  {{ formatNumber(report[key]) }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        
-        <!-- Minimized version of the table -->
-        <table v-if="isMinimized[stockList[0].Symbol]" class="stock-table-minimized">
-          <thead>
-            <tr>
-              <th>Last 5 reports for {{ stockList[0].Symbol }}</th>
-              <th>PE Ratio</th>
-              <th>Debt To Equity</th>
-              <th>Dividend Yield</th>
-              <th>Revenue</th>
-              <th>Payout Ratio</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>  </td>
-              <td>{{ formatNumber(stockList[0].PeRatio) }}</td>
-              <td>{{ formatNumber(stockList[0].DebtToEquity) }}</td>
-              <td>{{ formatNumber(stockList[0].DividendYield) }}</td>
-              <td>{{ formatNumber(stockList[0].Revenue) }}</td>
-              <td>{{ formatNumber(stockList[0].PayoutRatio) }}</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <!-- Maximized version of the table -->
-
-        <table v-else>
-          <thead>
-            <tr>
-              <th>Last 5 reports for {{ stockList[0].Symbol }}</th>
-              <th v-for="report in stockList" :key="report.Date">
-                {{ report.Date }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-
-            </tr>
-            <tr v-for="(value, key) in stockList[0]" :key="key">
-              <td>{{ key }}</td>
-              <td v-for="(report, reportIndex) in stockList" :key="reportIndex">
-                {{ formatNumber(report[key]) }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
-  </div>
-</template>
-<script>
-import { reactive, toRefs, computed } from 'vue';
-
-export default {
-  name: 'TableComponent',
-  props: {
-    incomeAndMetrics: Array,
-  },
-  setup(props) {
-    
-    // Setup reactive state
-    const state = reactive({
-      isMinimized: {},
-      // Computed property to transform props.incomeAndMetrics into a reactive stocks array
-      stocks: computed(() => props.incomeAndMetrics.map(stock => ({
-        ...stock, // Spread operator to copy properties from each stock object
-        // You can add additional transformations or computed properties for each stock here
-      }))),
-     
-    });
-
-    // Function to toggle the visibility of a table based on its stock symbol
-    function toggleTable(symbol) {
-      if (state.isMinimized[symbol] === undefined) {
-        // Initialize as false if it doesn't exist
-        state.isMinimized[symbol] = false;
+  </template>
+  <script>
+  import { reactive, toRefs, computed } from 'vue';
+  
+  export default {
+    name: 'TableComponent',
+    props: {
+      incomeAndMetrics: Array,
+    },
+    setup(props) {
+      
+      // Setup reactive state
+      const state = reactive({
+        isMinimized: {},
+        // Computed property to transform props.incomeAndMetrics into a reactive stocks array
+        stocks: computed(() => props.incomeAndMetrics.map(stock => ({
+          ...stock, // Spread operator to copy properties from each stock object
+          // You can add additional transformations or computed properties for each stock here
+        }))),
+       
+      });
+  
+      // Function to toggle the visibility of a table based on its stock symbol
+      function toggleTable(symbol) {
+        if (state.isMinimized[symbol] === undefined) {
+          // Initialize as false if it doesn't exist
+          state.isMinimized[symbol] = false;
+        }
+        state.isMinimized[symbol] = !state.isMinimized[symbol];
       }
-      state.isMinimized[symbol] = !state.isMinimized[symbol];
-    }
-
-    // Function to add a stock to localStorage
-    function addToLocalStorage(stock) {
-      let savedStocks = JSON.parse(localStorage.getItem('savedStocks')) || [];
-      savedStocks.push(stock);
-      localStorage.setItem('savedStocks', JSON.stringify(savedStocks));
-    }
-
-    // Function to remove a stock from localStorage by index
-    function removeFromLocalStorage(index) {
-      let savedStocks = JSON.parse(localStorage.getItem('savedStocks')) || [];
-      if (index >= 0 && index < savedStocks.length) {
-        savedStocks.splice(index, 1);
+  
+      // Function to add a stock to localStorage
+      function addToLocalStorage(stock) {
+        let savedStocks = JSON.parse(localStorage.getItem('savedStocks')) || [];
+        savedStocks.push(stock);
         localStorage.setItem('savedStocks', JSON.stringify(savedStocks));
       }
-    }
-
-    // Function to format numbers, optionally adding a currency symbol
-    function formatNumber(value, currency = '') {
-      if (typeof value === 'number') {
-        if (Math.abs(value) >= 1000000) {
-          return `${(value / 1e6).toFixed(2)}M ${currency}`;
+  
+      // Function to remove a stock from localStorage by index
+      function removeFromLocalStorage(index) {
+        let savedStocks = JSON.parse(localStorage.getItem('savedStocks')) || [];
+        if (index >= 0 && index < savedStocks.length) {
+          savedStocks.splice(index, 1);
+          localStorage.setItem('savedStocks', JSON.stringify(savedStocks));
         }
-        return value.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + ` ${currency}`;
       }
-      return value;
-    }
-
-    // Return reactive state and methods to the template
-    return {
-      ...toRefs(state), // Convert the reactive state to refs for use in the template
-      toggleTable,
-      addToLocalStorage,
-      removeFromLocalStorage,
-      formatNumber,
-    };
-  },
-};
-</script>
+  
+      // Function to format numbers, optionally adding a currency symbol
+      function formatNumber(value, currency = '') {
+        if (typeof value === 'number') {
+          if (Math.abs(value) >= 1000000) {
+            return `${(value / 1e6).toFixed(2)}M ${currency}`;
+          }
+          return value.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + ` ${currency}`;
+        }
+        return value;
+      }
+  
+      // Return reactive state and methods to the template
+      return {
+        ...toRefs(state), // Convert the reactive state to refs for use in the template
+        toggleTable,
+        addToLocalStorage,
+        removeFromLocalStorage,
+        formatNumber,
+      };
+    },
+  };
+  </script>
 
 
 <!-- table.vue -->
@@ -147,12 +146,21 @@ export default {
   border-collapse: collapse;
 }
 
+table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid black;
+
+}
+
 .stock-table-container {
   overflow-x: auto;
 }
+
 .stock-table-minimized {
   width: 775px;
 }
+
 .stock-table {
   display: flex;
   justify-content: center;
